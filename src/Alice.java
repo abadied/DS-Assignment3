e1e22c1d201d93d5fcd202cbbf95fbecc820c99c
abadied <abadi.eden@gmail.com>
2016-05-04 12:52:00 +0300

    alice&bob

--- src/Alice.java
+++ src/Alice.java
@@ -7,6 +7,7 @@ public class Alice {
     //Add Fields if needed
 
     public Alice(){
+    	puzzlesArray=null;
     }
 
     public Puzzle[] getPuzzlesCopy(){
@@ -18,7 +19,29 @@ public class Alice {
     }
 
     public void createPuzzles(int n, int k){
-        //Complete Your Code Here
+    	puzzlesArray=new Puzzle[k];
+    	int[] key,riddle;
+    	for (int i=0 ; i < k ; i++){
+    		key = new int[n];
+    		riddle = new int[n];
+    		for (int j=0 ; j < n ; j++){
+    			key[j] = (int)((j+Math.random())*Math.pow(n, 3));
+    			riddle[j] = (int)((j+Math.random())*Math.pow(n, 3));
+    		}
+    		puzzlesArray[i] = new Puzzle(key,riddle);
+    	}
+    	//TODO: test
+    }
+    
+    private void RandomShuffle(int[] arr){
+    	int n = arr.length;
+    	int temp,loc;
+    	for (int i=0 ; i<n ; i++){
+    		temp = arr[i];
+    		loc = (int)((Math.random()*(n-i))+i);
+    		arr[i] = arr[loc];
+    		arr[loc] = temp;
+    	}
     }
 
     public Pair<String, Integer> findKey(String sIndex){
--- src/Bob.java
+++ src/Bob.java
@@ -5,17 +5,63 @@ import java.io.*;
 public class Bob {
 
     //Add Fields if needed
+	//TODO:check if save of key and riddle needed
 
     public Bob(){
         //Complete Your Code Here
     }
 
     public Pair<String, String> choosePuzzle(Puzzle[] puzzles){
-		//Complete Your Code Here
+		int n = puzzles.length;
+		Puzzle p = puzzles[(int)(Math.random()*n)];
+		return solvePuzzle(p);
     }
 
     public Pair<String, String> solvePuzzle(Puzzle puz){
-        //Complete Your Code Here
+    	int[] sRiddle = sortPuzzle(puz.getRiddle());
+    	int[] sPrivateKey = sortPuzzle(puz.getPrivateKey());
+    	
+    	return new Pair<String, String>(arrXOR(sRiddle),arrXOR(sPrivateKey));
     }
-
+    
+    private int[] sortPuzzle(int[] arr){
+    	int n=arr.length;
+    	int[] sortedArr=new int[n];
+    	for(int i=0;i<n;i++){
+    		int loc=(int)(arr[i]/Math.pow(n, 3));
+    		sortedArr[loc]=arr[i];
+    	}
+    	return sortedArr;
+    }
+    
+    private int XOR(int a, int b){
+    	if (a!=b)
+    		return 1;
+    	return 0;
+    }
+    
+    private int intXOR(int num){
+    	int curr = num%2;
+    	num = num/2;
+    	while (num >= 1){
+    		curr = XOR(curr,num%2);
+    		num = num/2;
+    	}
+    	return curr;
+    }
+    
+    public String arrXOR(int[] arr){
+    	String s="";
+    	for (int i=0 ; i<arr.length ; i++)
+    		s += intXOR(arr[i]);
+    	return s;
+    }
+    
+    public static void main(String[] args){
+    	Bob b=new Bob();
+    	int[] arr = new int[2];
+    	arr[0]=5;
+    	arr[1]=10;
+    	System.out.println(b.arrXOR(arr));
+    }
 }
