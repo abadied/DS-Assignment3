92fbd2422188a80cd5751d5b0573214d15b955ac
abadied <abadi.eden@gmail.com>
2016-05-09 20:56:49 +0300

    test added

--- src/MerklePuzzles.java
+++ src/MerklePuzzles.java
@@ -7,6 +7,11 @@ public class MerklePuzzles {
     private Eve eve;
     String key;
     String privateKey;
+    
+    public long aliceCheckTime;
+    public long eveCheckTime;
+    public int aliceCompCount;
+    public int eveCompCount;
 
     public MerklePuzzles(){
         alice = new Alice();
@@ -40,7 +45,9 @@ public class MerklePuzzles {
         Pair<String,Integer> aliceAns = alice.findKey(key);
         long endTime = System.nanoTime();
         System.out.println("The private key of the puzzle Alice found: " + aliceAns);
+        aliceCompCount = aliceAns.getValue();
         System.out.println("The number of comparisons Alice had done: " + aliceAns.getValue());
+        aliceCheckTime = endTime - startTime;
         System.out.println("The time it took Alice to find the puzzle: " + (endTime - startTime));
     }
 
@@ -50,7 +57,9 @@ public class MerklePuzzles {
         Pair<String,Integer> eveAns = eve.findKey(key,puz);
         long endTime = System.nanoTime();
         System.out.println("The private key of the puzzle Eve found: " + eveAns.getKey());
+        eveCompCount = eveAns.getValue();
         System.out.println("The number of comparisons Eve had done: " + eveAns.getValue());
+        eveCheckTime = endTime - startTime;
         System.out.println("The time it took Eve to find the puzzle: " + (endTime - startTime));
     }
 
@@ -63,7 +72,19 @@ public class MerklePuzzles {
 
     public static void main(String args[]){
     	MerklePuzzles mp = new MerklePuzzles();
-    	mp.fullTest(10, 10);
+    	long ACT=0,ECT=0;
+    	int ACC=0,ECC=0;
+    	for (int i=0 ; i<10 ; i++){
+    		mp.fullTest(130, 140000);
+    		ACT+=mp.aliceCheckTime;
+    		ECT+=mp.eveCheckTime;
+    		ACC+=mp.aliceCompCount;
+    		ECC+=mp.eveCompCount;
+    	}
+    	System.out.println(ACC/10);
+    	System.out.println(ACT/10);
+    	System.out.println(ECC/10);
+    	System.out.println(ECT/10);
     }
 
 }
